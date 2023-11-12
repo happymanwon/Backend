bddf2965404edabc5c12cf4ab4ae35e557e85cf7
hyun <vikim1210@naver.com>
2023-11-12 17:17:50 +0900

    [Chore] 프로젝트 grdle 설정

---
+++ src/test/java/com/backend/hmanwon/HmanwonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.hmanwon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmanwonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
