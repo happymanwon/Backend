1dd94f909899cc3c4a92d0e22cc966b756e72f39
sungjiwoon <nebulaisme@naver.com>
2023-11-17 01:33:23 +0900

    엔티티 설정

--- src/main/java/org/hmanwon/domain/display/entity/Item.java
+++ src/main/java/org/hmanwon/domain/display/entity/Item.java
@@ -1,5 +1,43 @@
 package org.hmanwon.domain.display.entity;
 
-public class Item {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    @Comment("업소 아이템 id")
+    private Long id;
+
+    @Column(nullable = false)
+    @Comment("Item 이름")
+    private String itemName;
+
+    @Column(nullable = false)
+    @Comment("Item 가격")
+    private Integer itemPrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seoul_good_shop_id")
+    @Comment("서울 착한 업소 FK")
+    private SeoulGoodShop seoulGoodShop;
 }
