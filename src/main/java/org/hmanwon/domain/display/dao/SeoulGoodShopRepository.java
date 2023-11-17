c02527caafe59a8720f9629d078084b3155b0c75
HyunA <vikim1210@naver.com>
2023-11-18 03:09:37 +0900

    [Feature] Json 데이터 가공 및 스프링 카멜로 하되 Json은 스네이크

--- src/main/java/org/hmanwon/domain/display/dao/SeoulGoodShopRepository.java
+++ src/main/java/org/hmanwon/domain/display/dao/SeoulGoodShopRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SeoulGoodShopRepository extends JpaRepository<SeoulGoodShop, Long> {
-    List<SeoulGoodShop> findByShopCategoryId(Long category);
+    List<SeoulGoodShop> findByCategory(Long category);
 }
--- src/main/java/org/hmanwon/domain/display/dto/SeoulGoodShopResponse.java
+++ src/main/java/org/hmanwon/domain/display/dto/SeoulGoodShopResponse.java
@@ -1,4 +1,4 @@
-package org.hmanwon.domain.display.init.dto;
+package org.hmanwon.domain.display.dto;
 
 import com.google.gson.JsonElement;
 import lombok.Builder;
@@ -7,30 +7,23 @@ import lombok.Setter;
 import org.hmanwon.domain.display.entity.SeoulGoodShop;
 
 @Getter
-@Setter //나중에 처리
 @Builder
-public class SeoulGoodShopDTO {
+public class SeoulGoodShopResponse {
     private String way;
     private String imageUrl;
-    private String info;
-    private String phone;
     private String name;
     private Long category;
-    private int rcmnCnt;
-    private String pride;
     private String address;
 
     /*menu는 보류하고 나중에 수정하겠습니다*/
     private JsonElement menu;
 
-    public static SeoulGoodShopDTO fromEntity(SeoulGoodShop seoulGoodShop){
-        return SeoulGoodShopDTO.builder()
+    public static SeoulGoodShopResponse fromEntity(SeoulGoodShop seoulGoodShop){
+        return SeoulGoodShopResponse.builder()
             .way(seoulGoodShop.getWay())
             .imageUrl(seoulGoodShop.getImageUrl())
-            .info(seoulGoodShop.getInfo())
             .name(seoulGoodShop.getName())
             .category(seoulGoodShop.getCategory())
-            .pride(seoulGoodShop.getPride())
             .address(seoulGoodShop.getAddress())
             .build();
     }
