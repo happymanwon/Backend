9bc1658eff04def371fbd75772dea3a9150ecd56
HyunA <vikim1210@naver.com>
2023-11-17 20:53:03 +0900

    [Feature] crossOrigin 및 포트 변경

--- src/main/java/org/hmanwon/domain/display/api/DisplayController.java
+++ src/main/java/org/hmanwon/domain/display/api/DisplayController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.hmanwon.domain.display.application.DisplayService;
 import org.hmanwon.domain.display.init.dto.SeoulGoodShopDTO;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +15,14 @@ public class DisplayController {
     private final DisplayService displayService;
 
     @GetMapping("/api/stores")
+    @CrossOrigin("http://localhost")
     public List<SeoulGoodShopDTO> getAllStores(){
         return displayService.getAllStores();
     }
 
     //지금은 Category가 Long이지만 나중엔 Category 클래스로 뺄거임
     @GetMapping("/api/stores/{category}")
+    @CrossOrigin("http://localhost")
     public List<SeoulGoodShopDTO> getCategoryStores(
         @PathVariable final Long category
     ){
