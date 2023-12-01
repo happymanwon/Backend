f4f132b1e47e135541060a1c68188a90d630b2dc
sungjiwoon <nebulaisme@naver.com>
2023-12-01 22:45:52 +0900

    [fix] shopSerivce 위치 & 카테고리별 검색 가능하도록 수정

--- src/main/java/org/hmanwon/domain/shop/application/ShopService.java
+++ src/main/java/org/hmanwon/domain/shop/application/ShopService.java
@@ -1,11 +1,17 @@
 package org.hmanwon.domain.shop.application;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.hmanwon.domain.shop.dao.LocalCodeRepository;
 import org.hmanwon.domain.shop.dao.SeoulGoodShopRepository;
 import org.hmanwon.domain.shop.dto.SeoulGoodShopDetailResponse;
 import org.hmanwon.domain.shop.dto.SeoulGoodShopResponse;
+import org.hmanwon.domain.shop.entity.LocalCode;
+import org.hmanwon.domain.shop.entity.SeoulGoodShop;
+import org.hmanwon.domain.shop.exception.ShopException;
+import org.hmanwon.domain.shop.exception.ShopExceptionCode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,16 +20,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class ShopService {
 
     private final SeoulGoodShopRepository seoulGoodShopRepository;
+    private final LocalCodeRepository localCodeRepository;
 
     @Transactional(readOnly = true)
     public List<SeoulGoodShopResponse> getAllShops() {
         return seoulGoodShopRepository.findAll()
-            .stream().map(SeoulGoodShopResponse::fromEntity)
-            .collect(Collectors.toList());
-    }
-
-    public List<SeoulGoodShopResponse> getCategoryShops(Long category) {
-        return seoulGoodShopRepository.findByCategory(category)
             .stream().map(SeoulGoodShopResponse::fromEntity)
             .collect(Collectors.toList());
     }
@@ -40,4 +41,43 @@ public class ShopService {
             .collect(Collectors.toList());
     }
 
+    public List<SeoulGoodShopResponse> getShopsByCategoryAndLocalCode(
+            Long categoryId, Long localCode
+    ) {
+        if (localCode == null && categoryId == null) {
+            return getShopsByCategory(1L); //default
+        }
+        if (localCode == null) {
+            return getShopsByCategory(categoryId);
+        }
+        if (categoryId == null) {
+            return getShopsByLocalCode(localCode);
+        }
+
+        LocalCode location = getLocalCode(localCode);
+        return location.getSeoulGoodShopList()
+                .stream().filter(shop -> shop.getCategory() == categoryId)
+                .map(SeoulGoodShopResponse::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    public List<SeoulGoodShopResponse> getShopsByCategory(Long categoryId) {
+        return seoulGoodShopRepository.findByCategory(categoryId)
+                .stream().map(SeoulGoodShopResponse::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    public List<SeoulGoodShopResponse> getShopsByLocalCode(Long localCode) {
+        LocalCode location = getLocalCode(localCode);
+        return location.getSeoulGoodShopList()
+                .stream()
+                .map(SeoulGoodShopResponse::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    private LocalCode getLocalCode(Long localCode) {
+        return localCodeRepository.findById(localCode).orElseThrow(
+                () -> new ShopException(ShopExceptionCode.LOCAL_CODE_BAD_REQUEST)
+        );
+    }
 }
