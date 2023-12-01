bffe376c68b1c0478797b053dd69d61f7832ca33
sungjiwoon <nebulaisme@naver.com>
2023-12-01 22:45:55 +0900

    [fix] shopSerivce 위치 & 카테고리별 검색 가능하도록 수정

--- src/main/java/org/hmanwon/domain/shop/presentation/ShopController.java
+++ src/main/java/org/hmanwon/domain/shop/presentation/ShopController.java
@@ -5,6 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.hmanwon.domain.shop.application.ShopService;
 import org.hmanwon.domain.shop.dto.SeoulGoodShopDetailResponse;
 import org.hmanwon.domain.shop.dto.SeoulGoodShopResponse;
+import org.hmanwon.domain.shop.exception.ShopException;
+import org.hmanwon.domain.shop.exception.ShopExceptionCode;
+import org.hmanwon.global.common.dto.ResponseDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,17 +22,22 @@ public class ShopController {
 
     private final ShopService shopService;
 
-    @GetMapping
-    public List<SeoulGoodShopResponse> getAllShops() {
-        return shopService.getAllShops();
+    @GetMapping("")
+    public ResponseDTO<List<SeoulGoodShopResponse>> getShopsByCategoryAndLocalCode(
+            @RequestParam(name = "categoryId", required = false) final Long categoryId,
+            @RequestParam(name = "localCode", required = false) final Long localCode
+    ) {
+        if (categoryId != null && categoryId > 8) {
+            throw new ShopException(ShopExceptionCode.CATEGORY_BAD_REQUEST);
+        }
+        return ResponseDTO.res(HttpStatus.OK,
+                shopService.getShopsByCategoryAndLocalCode(
+                    categoryId, localCode
+                ),
+                "착한 가격 업소 조회 완료"
+        );
     }
 
-    @GetMapping("/categories/{categoryId}")
-    public List<SeoulGoodShopResponse> getCategoryShops(
-        @PathVariable final Long categoryId
-    ) {
-        return shopService.getCategoryShops(categoryId);
-    }
 
     @GetMapping("/{shopId}")
     public List<SeoulGoodShopDetailResponse> getShopDetail(
