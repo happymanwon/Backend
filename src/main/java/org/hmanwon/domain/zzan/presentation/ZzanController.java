c095d7794fb59b6e983d577129ba259908ebd76f
sungjiwoon <nebulaisme@naver.com>
2023-12-07 15:12:35 +0900

    [fix] 컨트롤러 api url 수정

--- src/main/java/org/hmanwon/domain/zzan/presentation/ZzanController.java
+++ src/main/java/org/hmanwon/domain/zzan/presentation/ZzanController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/zzan")
+@RequestMapping("/api/zzan-items")
 @RequiredArgsConstructor
 public class ZzanController {
 
