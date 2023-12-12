f1702b06f66a6611aa152f281f9f1b1a515b195c
HyunA <vikim1210@naver.com>
2023-12-13 02:19:50 +0900

    [Chore] CORS 설정

--- src/main/java/org/hmanwon/global/config/WebConfig.java
+++ src/main/java/org/hmanwon/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000","https://happy-manwon.vercel.app/")
+            .allowedOrigins("http://localhost:3000","https://happy-manwon.vercel.app")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
