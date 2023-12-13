0899a870fdbcade3cf617e847ad011e691173e2b
HyunA <vikim1210@naver.com>
2023-12-14 04:50:07 +0900

    [Feature] 외장 톰캣 사용 위해 war로 배포

--- src/main/java/org/hmanwon/HManwonApplication.java
+++ src/main/java/org/hmanwon/HManwonApplication.java
@@ -2,14 +2,21 @@ package org.hmanwon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication()
-public class HManwonApplication {
+public class HManwonApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HManwonApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(HManwonApplication.class);
     }
 
 }
